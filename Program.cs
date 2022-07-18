25b211f0be981aed572397cbae9e2d90ca53e9ee
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-19 00:36:27 +0200

    ok

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace T09_Ejercicio7
+{
+    class Persona
+    {
+        private string nombre;
+        private int edad;
+        private string dni;
+        //No sé si era esta forma como pedía el ejercicio la constante.
+        private string sexo = "H";
+        private double peso;
+        private double altura;
+
+        public Persona()
+        {
+            nombre = "";
+            edad = 0;
+            peso = 0;
+            altura = 0;
+        }
+
+        public Persona(string nombreConstr, int edadConstr, string sexoConstr)
+        {
+            nombre = nombreConstr;
+            edad = edadConstr;
+            sexo = sexoConstr;
+            peso = 0;
+            altura = 0;
+        }
+
+        public Persona(string nombreConstr, int edadConstr, string dniConstr, string sexoConstr, double pesoConstr, double alturaConstr)
+        {
+            nombre = nombreConstr;
+            edad = edadConstr;
+            sexo = sexoConstr;
+            peso = pesoConstr;
+            altura = alturaConstr;
+            dni = dniConstr;
+        }
+    }
+}
